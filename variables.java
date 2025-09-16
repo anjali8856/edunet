ce4bcc65a07028c7f5bdbbd876b3b27f80467e91
anjali8856 <kompellyanjali8@gmail.com>
2025-09-16 11:03:02 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,16 @@
+public class variables {
+    public static void main(String[] args) {
+        //declaring variables
+        int a=12;
+        int b=10;
+        //other way
+       int x=5,y=10;
+       //another way
+       int c,d,e;
+       c = d = e =10;
+        System.out.println(a);
+        System.out.println(a+b);
+        System.out.println(x+y);
+        System.out.println(c+d+e);
+    }
+}
